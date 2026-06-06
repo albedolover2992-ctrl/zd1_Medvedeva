ce11488946e5aaa54e1e0729f84c05cf725ecef2
albedolover2992-ctrl <albedolover2992@gmail.com>
2026-06-06 10:28:54 +0500

    Add files via upload

---
+++ практика_1_задание_1/Cat.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практика_задание_1
+{
+    class Cat
+    {
+        // Поля
+        private string name;
+        private double weight;
+
+        List<Cat> cats = new List<Cat>();  // Лист для котов вводимых в цикле
+
+        public Cat(string CatName, double CatWeight)
+        {
+            Name = CatName;
+            Weight = CatWeight;
+            cats.Add(this);
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine($"{name}: МЯУ ");
+        }
+
+        public void Info ()
+        {
+            Console.WriteLine($"{name} - {weight}");
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                bool OnlyLetters = true;
+
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                if (OnlyLetters)
+                    name = value;
+                else
+                    Console.WriteLine($"{value} - неправильное имя");
+            }
+        }
+
+        public double Weight
+        {
+            get { return weight; }
+            set
+            {
+                bool Right = false ;
+                if (value > 0 && value < 25)
+                {
+                    Right = true;
+                }
+
+                if (Right)
+                    weight = value;
+                else
+                {
+                    Console.WriteLine($"{value} - неверное значение");
+                    weight = 5.0;
+                }
+
+            }
+        }
+    }
+}
---
+++ практика_1_задание_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace практика_задание_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите сколько будет котов: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < n; i++)
+                {
+                    Cat cat = new Cat("", 0);
+                    Console.Write("Введите имя кота: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Введите вес кота: ");
+                    double weight = double.Parse(Console.ReadLine());
+                    cat.Name = name;
+                    cat.Weight = weight;
+                    cat.Meow();
+                    cat.Info();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Неверный формат для веса");
+            }
+        }
+    }
+}
