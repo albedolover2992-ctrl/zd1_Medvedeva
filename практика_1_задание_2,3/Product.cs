8363e9dda08903a559f4f3374b83581275fb7ef8
albedolover2992-ctrl <albedolover2992@gmail.com>
2026-06-08 03:41:12 +0500

    Product.cs

--- практика_1_задание_2,3/Product.cs
+++ практика_1_задание_2,3/Product.cs
@@ -1,25 +1,42 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace практика_1_задание_2
 {
     class Product
     {
-        public decimal Price { get; set; }
-        public string Name { get; set; }
+        public decimal price;
+        public string name;
+        public int count;
 
-        public Product(string Name, decimal Price)
+        public Product(string Name, decimal Price, int count)
         {
             this.Name = Name;
             this.Price = Price;
+            this.Count = count;
         }
 
-        public string GetInfo()
+        public decimal Price // свойство цены
         {
-            return $"Наименование: {Name}; Цена: {Price} руб.";
+            get { return price; }
+            set { price = value; }
+        }
+
+        public string Name // свойство названия
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Count // свойство кол-ва товара
+        {
+            get { return count; }
+            set { count = value; }
         }
     }
 }
